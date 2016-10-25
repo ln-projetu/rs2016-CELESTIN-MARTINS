bf11010ff40701db009dae7f0db03f4dca6c9d47
Emanuel MARTINS <manu_57190@hotmail.fr>
2016-10-25 15:25:06 +0200

    getopt

--- obj/main.c
+++ obj/main.c
@@ -5,40 +5,60 @@
 #include <fcntl.h>
 
 struct header_posix_ustar {
-	char name[100];
-	char mode[8];
-	char uid[8];
-	char gid[8];
-	char size[12];
-	char mtime[12];
-	char checksum[8];
-	char typeflag[1];
-	char linkname[100];
-	char magic[6];
-	char version[2];
-	char uname[32];
-	char gname[32];
-	char devmajor[8];
-	char devminor[8];
-	char prefix[155];
-	char pad[12];
+    char name[100];
+    char mode[8];
+    char uid[8];
+    char gid[8];
+    char size[12];
+    char mtime[12];
+    char checksum[8];
+    char typeflag[1];
+    char linkname[100];
+    char magic[6];
+    char version[2];
+    char uname[32];
+    char gname[32];
+    char devmajor[8];
+    char devminor[8];
+    char prefix[155];
+    char pad[12];
 }ma_struct;
 
-
-struct struct_interm{
-	int lu;
-	int size;
-	int fd;
-	struct header_posix_ustar ma_struct;
-	char* name;
-};
-
 int main (int argc, char* argv[]){
-	char* archive=argv[1];
-	int fd=open(archive,O_RDONLY);
-	listeur(fd, ma_struct,archive);
-	close(fd);
-
-	return(0);
+    int opt;
+    int i=1;
+    int option=0;
+    int fd;
+    while ((opt=getopt(argc, argv, "xlpz:"))!=-1){
+        switch(opt){
+            case 'x':
+                option=1;
+                i++;
+                printf("extrait le contenu de %s\n",argv[i]);
+                break;
+            case 'l':
+                if (option==0) i++;
+                option=1;
+                printf("listing détaillé de %s\n",argv[i]);
+                break;
+            case 'p':
+                if (option==0)i++;
+                option=1;
+                printf("utilisation de %s threads \n",argv[i]);
+                i++;
+                break;
+            case 'z':
+                if (option==0) i++;
+                option=1;
+                printf("décompression de %s\n",argv[i]);
+                break;
+            default:
+                printf("dans default" );
+                fd=open(argv[i],O_RDONLY);
+                listeur(fd, ma_struct,argv[i]);
+                close(fd);
+                exit(0);
+        }
+    }
+    return(0);
 }
-
