4b299fbf0fef2043e25cba097367a03c441e5ba1
Antonin <ninotna@osboxes>
2016-10-26 15:36:50 +0200

    ajout fork pour fichiers

--- obj/extracteur.c
+++ obj/extracteur.c
@@ -12,26 +12,25 @@
 
 
 void listeur(int fd, struct header_posix_ustar ma_struct,char* archive){
-	int lu=read(fd,&ma_struct,512);
+	int lu;
 	int size;
 	printf("Liste des dossiers et fichiers dans l'archive \"%s\" :\n", archive);
-	printf("-%s\n",ma_struct.name);
-	while (lu!=0){
+	do{
 		size=convert_oct_to_dec(ma_struct.size);
 		if (size!=0)
 			lseek(fd,(size/512+1)*512,SEEK_CUR);
 		lu=read(fd,&ma_struct,512);
 		if (strlen(ma_struct.name)!=0)
 			printf("-%s\n",ma_struct.name);
-	}
+	}while (lu!=0);
 }
 
-void extract(int fd, struct header_posix_ustar ma_struct, char *archive){
-	int lu=read(fd, &ma_struct,512);
+void extractDossier(int fd, struct header_posix_ustar ma_struct){
+	int lu;
 	int size;
 	int longueur;
 	//FILE *fichier=malloc(sizeof(FILE));
-	while(lu!=0){
+	do{
 		size=convert_oct_to_dec(ma_struct.size);
 		if (size!=0)
 			lseek(fd,(size/512+1)*512,SEEK_CUR);
@@ -40,9 +39,29 @@ void extract(int fd, struct header_posix_ustar ma_struct, char *archive){
 		if (longueur!=0){
 			if (ma_struct.name[longueur-1]=='/')
 				mkdir(ma_struct.name,0777);
-			else{
+			/*else{
+				execlp("touch","touch",ma_struct.name,NULL);
+			}*/
+		}
+	}while(lu!=0);
+}
 
+void extractFichier(int fd, struct  header_posix_ustar ma_struct){
+	int lu;
+	int size;
+	int longueur;
+	//FILE *fichier=malloc(sizeof(FILE));
+	do{
+		size=convert_oct_to_dec(ma_struct.size);
+		if (size!=0)
+			lseek(fd,(size/512+1)*512,SEEK_CUR);
+		lu=read(fd,&ma_struct.name,512);
+		longueur=strlen(ma_struct.name);
+		if (longueur!=0){
+			if (ma_struct.name[longueur-1]!='/'){
+				if (fork()==0)
+					execlp("touch","touch",ma_struct.name,NULL);
 			}
 		}
-	}
+	}while(lu!=0);
 }
